31123d44f4f8d9f54d7389c16746da2535eeab2f
Andrey Valitov <andreivalitov2001@gmail.com>
2019-10-25 12:01:18 +0700

    Add lab3-0

---
+++ lab3-0/cmake-build-debug/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: C:/template/lab3-0
+# Build directory: C:/template/lab3-0/cmake-build-debug
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(testlab3-0.exe "C:/template/lab3-0/test/testlab3-0.exe" "./lab3-0")
+set_tests_properties(testlab3-0.exe PROPERTIES  DEPENDS "lab3-0" _BACKTRACE_TRIPLES "C:/template/modules/test_runner.cmake;23;add_test;C:/template/modules/test_runner.cmake;0;;C:/template/modules/common_lab.cmake;3;include;C:/template/modules/common_lab.cmake;0;;C:/template/lab3-0/CMakeLists.txt;9;include;C:/template/lab3-0/CMakeLists.txt;0;")
